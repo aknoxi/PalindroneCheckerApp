59238a564946679c355a646e0f07c1d079f66614
aknoxi <ankitkumar0655ind@gmail.com>
2026-03-06 09:23:38 +0530

    ankit added uc2

--- app/src/PalindroneCheckerApp.java
+++ app/src/PalindroneCheckerApp.java
@@ -1,5 +1,22 @@
 public class PalindroneCheckerApp {
     public static void main (String[] args ){
+
         System.out.println("welcome to the palindrome checker app!!!");
+        String word = "madam";
+
+
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+
+        if(word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is not a Palindrome.");
+        }
+
     }
 }
